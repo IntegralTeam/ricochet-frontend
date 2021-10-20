c306477c748803bb4067feb931e2354849362b7c
Alexei Khasnudinov <akhasnudinov@sfxdx.ru>
2021-10-20 18:44:26 +0300

    add downgradeConfig

---
+++ src/constants/downgradeConfig.ts
@@ -0,0 +1,30 @@
+import {
+  USDCxAddress, 
+  WETHxAddress,
+  DAIxAddress,
+  MKRxAddress,
+  WBTCxAddress,
+} from './polygon_config';
+
+export const downgradeTokensList = [
+  {
+    coin: 'USDCx',
+    tokenAddress: USDCxAddress,
+  },
+  {
+    coin: 'DAIx',
+    tokenAddress: DAIxAddress,
+  },
+  {
+    coin: 'MKRx',
+    tokenAddress: MKRxAddress,
+  },
+  {
+    coin: 'WETHx',
+    tokenAddress: WETHxAddress,
+  },
+  {
+    coin: 'WBTCx',
+    tokenAddress: WBTCxAddress,
+  },
+];
