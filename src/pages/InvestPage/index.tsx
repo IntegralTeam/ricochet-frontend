aae660317a6fd7f1e992778367b9ebac9b8f2f19
Alexei Khasnudinov <akhasnudinov@sfxdx.ru>
2021-10-21 09:03:47 +0300

    replace mainLoadData to app container

--- src/pages/InvestPage/index.tsx
+++ src/pages/InvestPage/index.tsx
@@ -4,26 +4,18 @@ import { InvestContainer } from 'containers/main/InvestContainer';
 import { MainLayout } from 'containers/MainLayout';
 import { useShallowSelector } from 'hooks/useShallowSelector';
 import React, {
-  FC, useEffect,
+  FC,
 } from 'react';
-import { useDispatch } from 'react-redux';
-import { mainCheck } from 'store/main/actionCreators';
 import { selectMain } from 'store/main/selectors';
 import styles from './styles.module.scss';
 
 interface IProps {}
 
 const InvestPage: FC<IProps> = () => {
-  const dispatch = useDispatch();
-
   const {
     address,
     balances, 
   } = useShallowSelector(selectMain);
-
-  useEffect(() => {
-    dispatch(mainCheck());
-  }, [dispatch]);
 
   return (
     <MainLayout>
